02b29ace57f8b75e3c32a6ae286338818492d158
brittanymariea <brittanymariealdaco@gmail.com>
2018-12-05 19:28:53 -0700

    setting up pages and their corresponding components

---
+++ app/src/pages/MainPage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Container from "../component/Container/index";
+import Col from "../component/Col/index";
+import Row from "../component/Row/index";
+
+function Trail() {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        <h1> Welcome to! Trail Meet</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <h3>
+                            Trail
+                        </h3>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.   
+                            </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <h3>
+                            Trail
+                        </h3>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.   
+                            </p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Trail;
