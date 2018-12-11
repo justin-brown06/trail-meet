4aa5938619ccbea04d662357ac94a3a1e7ded001
Darlene1985 <Darlene_Leslie@ymail.com>
2018-12-11 13:38:28 -0700

    pull from master

--- app/src/components/modals/alert.js
+++ app/src/components/modals/alert.js
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
-import Modal from "./Modal";
-import { Card, Header, Content } from "../common/Bulma/Card";
-import { connect } from "react-redux";
-import { toggleModal } from "../../actions";
+// import React, { useState } from "react";
+// import Modal from "./Modal";
+// import { Card, Header, Content } from "../common/Bulma/Card";
+// import { connect } from "react-redux";
+// import { toggleModal } from "../../actions";
 
-class Login extends Component {
+// class Login extends Component {
 
-}
+// }
 
-render() {
-    return (
-        <Modal>
-            <Card>
-               <Header>Add Your Zipcode</Header>
-                <Content>
-                    <div className="notification">
-                        <button className="delete"></button>
-                        <div className="field">
-                            <div className="control">
-                                <input className="input is-success" type="text" placeholder="Enter Zipcode"></input>
-                            </div>
-                        </div>
-                      <input type="submit"> Submit </input>
-                    </div>
-                </Content>
-            </Card>
-        </Modal>
-    )
-};
+// render() {
+//     return (
+//         <Modal>
+//             <Card>
+//                <Header>Add Your Zipcode</Header>
+//                 <Content>
+//                     <div className="notification">
+//                         <button className="delete"></button>
+//                         <div className="field">
+//                             <div className="control">
+//                                 <input className="input is-success" type="text" placeholder="Enter Zipcode"></input>
+//                             </div>
+//                         </div>
+//                       <input type="submit"> Submit </input>
+//                     </div>
+//                 </Content>
+//             </Card>
+//         </Modal>
+//     )
+// };
---
+++ app/src/components/zipcode.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function location(props) {
+    return (
+        <div>
+            <input class="input" type="text" placeholder="Enter Zipcode" ></input>
+            <a class="button">Submit</a>
+        </div>
+    )
+}
