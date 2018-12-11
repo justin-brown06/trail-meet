15e6716a67a902785ef97d49efd6984c24290f32
Justin.B.Brown <brownee06@gmail.com>
2018-12-11 14:53:12 -0700

    Update to show trails based on users current location

--- app/src/components/pages/Home/API.js
+++ app/src/components/pages/Home/API.js
@@ -1,34 +1,42 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Geocode from "react-geocode";
+// import Geocode from "react-geocode";
 
 class API extends Component {
     constructor() {
         super();
         this.state = {
             trailNames: [],
+            lat: "",
+            lon: ""
         };
     };
 
 
     componentDidMount() {
-        axios.get('https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200394657-1ebddf3d823768d96c230dd00cd31c30')
-            .then((data) => {
-                console.log(data);
-                for (let i = 0; i < data.data.trails.length; i++) {
-                    console.log(data.data.trails[i].name)
-                };
 
-                let trailNames = data.data.trails.map((trail) => {
-                    return (
-                        <div key={trail.name}>
-                            <p> ${trail.name} </p>
-                        </div>
-                    )
+        navigator.geolocation.getCurrentPosition((position) => {
+            let location = "lat=" + position.coords.latitude +
+                "&lon=" + position.coords.longitude;
+                axios.get('https://www.hikingproject.com/data/get-trails?' + location + '&maxDistance=10&key=200394657-1ebddf3d823768d96c230dd00cd31c30')
+                .then((data) => {
+                    console.log(data);
+                    for (let i = 0; i < data.data.trails.length; i++) {
+                        console.log(data.data.trails[i].name)
+                    };
+
+                    let trailNames = data.data.trails.map((trail) => {
+                        return (
+                            <div key={trail.name}>
+                                <p> {trail.name} </p>
+                            </div>
+                        )
+                    });
+                    this.setState({ trailNames: trailNames });
+                    console.log("state", this.state.trailNames);
                 });
-                this.setState({ trailNames: trailNames });
-                console.log("state", this.state.trailNames);
-            });
+        });
+
     };
 
     render() {
