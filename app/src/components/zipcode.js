08f4d65bee922e663d9b8d9444bcb80996cbca23
Darlene1985 <Darlene_Leslie@ymail.com>
2018-12-14 14:31:03 -0700

    working zipcode.js

--- app/src/components/zipcode.js
+++ app/src/components/zipcode.js
@@ -1,40 +1,29 @@
 import React from "react";
 import axios from "axios";
 
-
+//zip code will override current location pull
  class ZipCode extends React.Component {
   state = {
-    zip: []
+    zip: ""
   }
 
-  componentDidMount() {
-    axios.get(`https://api.promaptools.com/service/us/zip-lat-lng/get/?zip=${this.state.zip}&key=zlysdycvm08e9fnk`)
-      .then(res => {
-        const zip = res.data;
-      })
+  handleInputChange = event => {
+    const { name, value } = event.target
+    this.setState({
+    [name]: value
+  });
   }
 
   getHikes(){
-    axios.get(`https://api.promaptools.com/service/us/zip-lat-lng/get/?zip=${this.state.zip}&key=zlysdycvm08e9fnk`)
-      .then(res => {
-        const zip = res.data;
-      })
-  }
-
-  //1. handleChange - model after sample code
-  //2. handle submit - instead of this.getBooks() have it call function #3
-  //3. getHikes - make a new function called getHikes that has the same axios call as the component did mount
-
-  handleInputChange(event) {
-    const { name, value } = event.target
-    this.setState({
-    [name]: value});
+    axios.get(`https://api.promaptools.com/service/us/zip-lat-lng/get/?zip=${this.state.zip}&key=zlysdycvm08e9fnk`) 
+    .then(res => 
+      console.log(res.data.output[0].latitude, res.data.output[0].longitude)
+    )
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
     console.log(event)
-    // this.setState({zip: event.target.value});
     this.getHikes();
   }
 
@@ -42,7 +31,7 @@ import axios from "axios";
   render() {
     return (
       <div>
-      <input id="zipcode" className="input" type="text" value={this.state.value} placeholder="Enter Zip Code" />
+      <input id="zipcode" className="input" value={this.state.zip} type="text" name="zip" onChange={this.handleInputChange} placeholder="Enter Zip Code" />
       <a className="button" onClick={this.handleSubmit} >Submit</a>
       </div>
     )
