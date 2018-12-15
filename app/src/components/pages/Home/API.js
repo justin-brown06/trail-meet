2721cf93ebedb47848a20ab47a56f694cf0be875
Justin.B.Brown <brownee06@gmail.com>
2018-12-14 22:41:16 -0700

    Update Table Font

--- app/src/components/pages/Home/API.js
+++ app/src/components/pages/Home/API.js
@@ -87,7 +87,10 @@ class API extends Component {
                                 <tr>
                                     <td>
                                         <Link to={`/${trail.id}`}> {trail.name}</Link>
-                                        <a target="_blank" rel="noopener noreferrer" href={"https://www.hikingproject.com/trail/" + trail.id}> <button className="button is-rounded is-dark is-small">Additional Information</button></a>
+                                        <a target="_blank" rel="noopener noreferrer" href={"https://www.hikingproject.com/trail/" + trail.id}>
+                                            <br />
+                                            <br />
+                                            <button className="button is-rounded is-dark is-small">Additional Information</button></a>
                                     </td>
                                     <td >{trail.difficulty}</td>
                                     <td>{trail.length}</td>
