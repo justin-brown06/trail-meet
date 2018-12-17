86a91fa031b32f0d424061c52fa18f0a4ab62525
brittanymariea <brittanymariealdaco@gmail.com>
2018-12-17 10:26:50 -0700

    merge with justin

--- app/src/components/pages/Home/API.js
+++ app/src/components/pages/Home/API.js
@@ -14,57 +14,71 @@ class API extends Component {
             trails: [],
             lat: "",
             lon: "",
+            zip: "",
+            location: []
         };
     };
 
     componentDidMount() {
 
         navigator.geolocation.getCurrentPosition((position) => {
-            let location = "lat=" + position.coords.latitude +
-                "&lon=" + position.coords.longitude;
-
-            axios.get('https://www.hikingproject.com/data/get-trails?' + location + '&maxDistance=10&key=200394657-1ebddf3d823768d96c230dd00cd31c30')
-                .then((data) => {
-                    console.log(data);
-                    for (let i = 0; i < data.data.trails.length; i++) {
-                        // console.log(data.data.trails[i].name)
-                    };
-                    this.setState({
-                        trails: data.data.trails
-                    });
-                });
-            //   axios.get("https://maps.googleapis.com/maps/api/js?key=AIzaSyD7XeO6If1j_8pp2FQeG7bgd6EUp-92ER0&callback=initMap") .then((geocoder, map, infowindow) => {
-            //         const lat = trail.latitude;
-            //         const lng = trail.longitude;
-            //         let latlngStr = lat + ", " + lng;
-            //         let latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-            //         geocoder.geocode({'location': latlng}, function(results, status){
-            //             if(status === "OK"){
-            //                 if(results[0]){
-            //                     map.setZoom(11);
-            //                     let marker = new google.maps.Marker({
-            //                         position: latlng,
-            //                         map: map
-            //                     })
-            //                     infowindow.setContent(result[0].formatted_address);
-            //                 }
-            //             }
-            //         })
-            //   })  
+            this.setState({
+                location: [position.coords.latitude, position.coords.longitude]
+            });
+            this.getHikes()
         });
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        });
+    }
+
+    getHikes() {
+        let location = "lat=" + this.state.location[0] +
+            "&lon=" + this.state.location[1];
+
+        axios.get('https://www.hikingproject.com/data/get-trails?' + location + '&maxDistance=10&key=200394657-1ebddf3d823768d96c230dd00cd31c30')
+            .then((data) => {
+                console.log(data);
+                for (let i = 0; i < data.data.trails.length; i++) {
+                    // console.log(data.data.trails[i].name)
+
+                };
+                this.setState({
+                    trails: data.data.trails
+                });
+            });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        axios.get(`https://api.promaptools.com/service/us/zip-lat-lng/get/?zip=${this.state.zip}&key=zlysdycvm08e9fnk`)
+            .then(res => {
+                this.setState({
+                    location: [res.data.output[0].latitude, res.data.output[0].longitude]
+                })
+                this.getHikes();
+            });
     };
 
     render() {
         return (
             <div className="App">
+                <div>
+                    <input id="zipcode" className="input" value={this.state.zip} type="text" name="zip" onChange={this.handleInputChange} placeholder="Enter Zip Code" />
+                    <button className="button" onClick={this.handleSubmit} >Submit</button>
+                </div>
                 <table className="table is-fullwidth">
                     <thead>
                         <tr>
-                            <th><abbr title="Trail">Trail Name</abbr></th>
-                            <th onClick={this.props.DifficultyModal}><abbr title="Difficulty"> <a className="is-white">Difficulty</a></abbr></th>
-                            <th><abbr title="Length">Length (miles)</abbr></th>
-                            <th><abbr title="Location">Location </abbr></th>
-                            <th><abbr title="image">Image</abbr></th>
+                            <th><span>Trail Name</span></th>
+                            <th onClick={this.props.DifficultyModal}><abbr title="Select for Difficulty Legend"> <span className="is-white">Difficulty</span></abbr></th>
+                            <th><span>Length (miles)</span></th>
+                            <th><span>Location </span></th>
+                            <th><span>Image</span></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -72,13 +86,15 @@ class API extends Component {
                             return (
                                 <tr>
                                     <td>
-                                        <Link to={`/${trail.id}`}> {trail.name}</Link>
-                                        <a target="_blank" rel="noopener noreferrer" href={"https://www.hikingproject.com/trail/" + trail.id}> {trail.name} </a>
+                                        <Link to={`/${trail.id}`}> <h1>{trail.name}</h1></Link>
+                                        <a target="_blank" rel="noopener noreferrer" href={"https://www.hikingproject.com/trail/" + trail.id}>
+                                            <br />
+                                            <button className="button is-rounded is-dark is-small">Additional Information</button></a>
                                     </td>
                                     <td >{trail.difficulty}</td>
                                     <td>{trail.length}</td>
                                     <td>{trail.latitude}, {trail.longitude}</td>
-                                    <td><img src={trail.imgSqSmall} alt=""/></td>
+                                    <td><img src={trail.imgSqSmall} alt="" /></td>
                                 </tr>
                             )
                         })}
