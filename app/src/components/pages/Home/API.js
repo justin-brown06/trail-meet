de44f40d62efaf203f962b6dbd9ef4a0643010fa
Artemis <35303907+slane1980@users.noreply.github.com>
2018-12-18 20:18:31 -0700

    alerts added for save and remove hike

--- app/src/components/pages/Home/API.js
+++ app/src/components/pages/Home/API.js
@@ -18,36 +18,39 @@ class API extends Component {
       location: [],
       address: []
     };
-  };
+  }
 
   componentDidMount() {
-
     navigator.geolocation.getCurrentPosition(position => {
       this.setState({
         location: [position.coords.latitude, position.coords.longitude]
       });
-      this.getHikes()
+      this.getHikes();
     });
-  };
+  }
 
   handleInputChange = event => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
     this.setState({
       [name]: value
     });
   };
 
   getHikes() {
-    let location = `lat=${this.state.location[0]}&lon=${this.state.location[1]}`;
+    let location = `lat=${this.state.location[0]}&lon=${
+      this.state.location[1]
+    }`;
 
     fetch(
-      "https://www.hikingproject.com/data/get-trails?" + location + "&maxDistance=10&key=200394657-1ebddf3d823768d96c230dd00cd31c30"
+      "https://www.hikingproject.com/data/get-trails?" +
+        location +
+        "&maxDistance=10&key=200394657-1ebddf3d823768d96c230dd00cd31c30"
     )
       .then(res => res.json())
       .then(data => {
         for (let i = 0; i < data.trails.length; i++) {
           // console.log(data.data.trails[i].name)
-          let coords = [data.trails[i].latitude, data.trails[i].longitude]
+          let coords = [data.trails[i].latitude, data.trails[i].longitude];
           // console.log(coords);
           this.getAddress(coords);
         }
@@ -64,52 +67,68 @@ class API extends Component {
         id
       })
       .then(res => {
-        // console.log(res.data);
+        alert("Hike has been saved");
       });
   };
   handleSubmit = event => {
     event.preventDefault();
-    fetch (
-        `https://api.promaptools.com/service/us/zip-lat-lng/get/?zip=${this.state.zip}&key=zlysdycvm08e9fnk`,
-        {
-          headers: { Accept: "application/json" }
-        }
-      )
+    fetch(
+      `https://api.promaptools.com/service/us/zip-lat-lng/get/?zip=${
+        this.state.zip
+      }&key=zlysdycvm08e9fnk`,
+      {
+        headers: { Accept: "application/json" }
+      }
+    )
       .then(res => res.json())
       .then(data => {
-        console.log(data)
+        console.log(data);
         this.setState({
           location: [data.output[0].latitude, data.output[0].longitude]
         });
         this.getHikes();
-      })
+      });
   };
 
   getAddress(coords) {
-    let location = `${coords[0]},${coords[1]}`
+    let location = `${coords[0]},${coords[1]}`;
     this.setState({
-      address:[]
-    })
+      address: []
+    });
 
-    fetch ("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + location + "&key=AIzaSyD7XeO6If1j_8pp2FQeG7bgd6EUp-92ER0")
-      .then( res => res.json())
-      .then((data) => {
+    fetch(
+      "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+        location +
+        "&key=AIzaSyD7XeO6If1j_8pp2FQeG7bgd6EUp-92ER0"
+    )
+      .then(res => res.json())
+      .then(data => {
         // console.log(data.results[0].formatted_address)
         this.setState({
           address: [...this.state.address, data.results[0].formatted_address]
-        })
+        });
       });
-  };
+  }
 
   render() {
     return (
       <div className="App">
-      <section className="level">
-      <div className="title level-left">Find a Trail!</div>
-        <div id="zip" className="level-right">
-          <input id="zipcode" className="input" value={this.state.zip} type="text" name="zip" onChange={this.handleInputChange} placeholder="Enter Zip Code" />
-          <button className="button is-info" onClick={this.handleSubmit} >Submit</button>
-        </div>
+        <section className="level">
+          <div className="title level-left">Find a Trail!</div>
+          <div id="zip" className="level-right">
+            <input
+              id="zipcode"
+              className="input"
+              value={this.state.zip}
+              type="text"
+              name="zip"
+              onChange={this.handleInputChange}
+              placeholder="Enter Zip Code"
+            />
+            <button className="button is-info" onClick={this.handleSubmit}>
+              Submit
+            </button>
+          </div>
         </section>
         <table className="table is-fullwidth">
           <thead>
@@ -117,11 +136,24 @@ class API extends Component {
               <th>
                 <span title="save">Save Hike</span>
               </th>
-              <th><span>Trail Name</span></th>
-              <th onClick={this.props.DifficultyModal}><abbr title="Select for Difficulty Legend"> <span className="is-white">Difficulty</span></abbr></th>
-              <th><span>Length (miles)</span></th>
-              <th><span>Location </span></th>
-              <th><span>Image</span></th>
+              <th>
+                <span>Trail Name</span>
+              </th>
+              <th onClick={this.props.DifficultyModal}>
+                <abbr title="Select for Difficulty Legend">
+                  {" "}
+                  <span className="is-white">Difficulty</span>
+                </abbr>
+              </th>
+              <th>
+                <span>Length (miles)</span>
+              </th>
+              <th>
+                <span>Location </span>
+              </th>
+              <th>
+                <span>Image</span>
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -138,24 +170,36 @@ class API extends Component {
                     </button>
                   </td>
                   <td>
-                    <Link to={`/${trail.id}`}> <h1>{trail.name}</h1></Link>
-                    <a target="_blank" rel="noopener noreferrer" href={"https://www.hikingproject.com/trail/" + trail.id}>
+                    <Link to={`/${trail.id}`}>
+                      {" "}
+                      <h1>{trail.name}</h1>
+                    </Link>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={"https://www.hikingproject.com/trail/" + trail.id}
+                    >
                       <br />
-                      <button className="button is-rounded is-dark is-small">Additional Information</button></a>
+                      <button className="button is-rounded is-dark is-small">
+                        Additional Information
+                      </button>
+                    </a>
                   </td>
-                  <td >{trail.difficulty}</td>
+                  <td>{trail.difficulty}</td>
                   <td>{trail.length}</td>
                   <td>{this.state.address[i]}</td>
-                  <td><img src={trail.imgSqSmall} alt="" /></td>
+                  <td>
+                    <img src={trail.imgSqSmall} alt="" />
+                  </td>
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
       </div>
-    )
-  };
-};
+    );
+  }
+}
 
 function mapDispatchToProps(dispatch) {
   return {
